cdc318dbc551ae77d23b1a360316d3bc6702330f
Aravind-Anbalagan <anbalagan.aravind@gmail.com>
2025-11-22 20:02:55 +0530

    Updated Scheduler

--- src/main/java/com/crumbs/trade/controller/SpreadController.java
+++ src/main/java/com/crumbs/trade/controller/SpreadController.java
@@ -2,6 +2,8 @@ package com.crumbs.trade.controller;
 
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
 import com.crumbs.trade.service.SpreadService;
@@ -14,6 +16,7 @@ public class SpreadController {
 	private final SpreadService spreadService;
 
 	@PostMapping("/execute")
+	@Scheduled(cron = "0 20 09 * * MON-FRI", zone = "Asia/Kolkata")
 	public void executeSpread() {
 		spreadService.getStockList();
 	}
