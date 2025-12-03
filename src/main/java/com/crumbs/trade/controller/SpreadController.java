8e66787ed23b55ed676bf24ad70ce64908a23df0
Aravind-Anbalagan <anbalagan.aravind@gmail.com>
2025-12-03 15:12:31 +0530

    Updated chart

--- src/main/java/com/crumbs/trade/controller/SpreadController.java
+++ src/main/java/com/crumbs/trade/controller/SpreadController.java
@@ -3,9 +3,11 @@ package com.crumbs.trade.controller;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
+import com.crumbs.trade.repo.StrategyRepo;
 import com.crumbs.trade.service.SpreadService;
 
 @RestController
@@ -15,9 +17,15 @@ public class SpreadController {
 
 	private final SpreadService spreadService;
 
+	@Autowired
+	StrategyRepo strategyRepo;
+	
 	@PostMapping("/execute")
 	@Scheduled(cron = "0 20 09 * * MON-FRI", zone = "Asia/Kolkata")
 	public void executeSpread() {
-		spreadService.getStockList();
+		if (strategyRepo.findByName("SPREAD_STRATEGY").getActive().equals("Y")) {
+			spreadService.getStockList();
+		}
+
 	}
 }
